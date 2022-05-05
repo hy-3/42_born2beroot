11338d164abe7cd1a607c232ae309b7fb9aeffc9
Hiroki Yamamoto <hymt73@gmail.com>
2022-05-05 19:35:10 +0200

    fixed to print when cron runs

--- monitoring.sh
+++ monitoring.sh
@@ -1,85 +1,73 @@
 #!/bin/bash
 
-###### Architecture ######
-echo "  # Architecture    : "
-echo -n "        - operating system :"
-hostnamectl | grep "Operating System" | cut -d':' -f2 2>/dev/null | tr -d '\n'
-echo ""
-echo -n "        - architecture     :"
-hostnamectl | grep "Architecture" | cut -d':' -f2 2>/dev/null | tr -d '\n'
-echo ""
-echo -n "        - kernel version   :"
-hostnamectl | grep "Kernel" | cut -d':' -f2 2>/dev/null | tr -d '\n'
-echo ""
+## Architecture ##
+OS=`hostnamectl | grep "Operating System" | cut -d':' -f2 2>/dev/null`
+ARCH=`hostnamectl | grep "Architecture" | cut -d':' -f2 2>/dev/null`
+KERNEL=`hostnamectl | grep "Kernel" | cut -d':' -f2 2>/dev/null`
 
-###### CPU physical ######
-echo -n "  # CPU physical    : "
-lscpu | grep "^CPU(s):" | awk '{print $2}' 2>/dev/null | tr -d '\n'
-echo ""
+## CPU physical ##
+CPU_PHYSI=`lscpu | grep "^CPU(s):" | awk '{print $2}' 2>/dev/null`
 
-###### vCPU ######
-echo -n "  # vCPU            : "
+## vCPU ##
 THREAD=`lscpu | grep "^Thread(s) per core:" | awk '{print $4}' 2>/dev/null | tr -d '\n'`
 CORE=`lscpu | grep "^Core(s) per socket:" | awk '{print $4}' 2>/dev/null | tr -d '\n'`
 CPU=`lscpu | grep "^CPU(s):" | awk '{print $2}' 2>/dev/null | tr -d '\n'`
-expr $THREAD \* $CORE \* $CPU 2>/dev/null | tr -d '\n'
-echo ""
+VCPU=`expr $THREAD \* $CORE \* $CPU 2>/dev/null`
 
-###### Memory Usage ######
-echo -n "  # Memory Usage    : "
+## Memory Usage ##
 USED_MEM_MB=`free --mega | grep "^Mem:" | awk '{print $3}' 2>/dev/null | tr -d '\n'`
 TOTAL_MEM_MB=`free --mega | grep "^Mem:" | awk '{print $2}' 2>/dev/null | tr -d '\n'`
-echo -n "$USED_MEM_MB/$TOTAL_MEM_MB[MB]"
-awk -v used=$USED_MEM_MB -v total=$TOTAL_MEM_MB 'BEGIN {printf(" (%.2f%%)", used/total*100)}' 2>/dev/null | tr -d '\n'
-echo ""
+MEM_PERCENT=`awk -v used=$USED_MEM_MB -v total=$TOTAL_MEM_MB 'BEGIN {printf(" (%.2f%%)", used/total*100)}' 2>/dev/null`
+MEMORY_USAGE="$USED_MEM_MB/$TOTAL_MEM_MB[MB] $MEM_PERCENT"
 
-###### Disk Usage ######
-echo -n "  # Disk Usage      : "
+## Disk Usage ##
 USED_DISK=`df -h / | awk 'FNR == 2 {print $3}' 2>/dev/null | tr -d '\n'`
 TOTAL_DISK=`df -h / | awk 'FNR == 2 {print $2}' 2>/dev/null | tr -d '\n'`
 USED_PERCENT=`df -h / | awk 'FNR == 2 {print $5}' 2>/dev/null | tr -d '\n'`
-echo -n "$USED_DISK/$TOTAL_DISK ($USED_PERCENT)"
-echo ""
+DISK_USAGE="$USED_DISK/$TOTAL_DISK ($USED_PERCENT)"
 
-###### CPU load ######
-echo -n "  # CPU load        : "
-top -bn1 | grep '^%Cpu(s)' | awk '{print $2 + $4 "%"}' 2>/dev/null | tr -d '\n'
-echo ""
+## CPU load ##
+CPU_LOAD=`top -bn1 | grep '^%Cpu(s)' | awk '{print $2 + $4 "%"}' 2>/dev/null`
 
-###### Last boot ######
-echo -n "  # Last boot       : "
-who -b | awk '{print $3 " " $ 4}' 2>/dev/null | tr -d '\n'
-echo ""
+## Last boot ##
+LAST_BOOT=`who -b | awk '{print $3 " " $ 4}' 2>/dev/null`
 
-###### LVM use ######
-echo -n "  # LVM use         : "
+## LVM use ##
+LVM_USE="no"
 COUNT=`cat /etc/fstab 2>/dev/null | grep "/dev/mapper" | wc -l`
 if [ $COUNT > 0 ]
 then
-	echo "yes"
-else
-	echo "no"
+	LVM_USE="yes"
 fi
 
-###### Connections TCP ######
-echo -n "  # Connections TCP : "
-last | grep "still logged in" | wc -l | tr -d '\n'
-echo " (ESTABLISHED)"
+## Connections TCP ##
+CONN_TCP=`last | grep "still logged in" | wc -l`
 
-###### User log ######
-echo -n "  # User log        : "
-who | wc -l
+## User log ##
+USER_LOG=`who | wc -l`
 
-###### Network ######
-echo "  # Network         : "
-echo -n "        - IPv4 address : "
-ip addr | grep "enp0s3" | awk 'FNR==2 {print $2}' 2>/dev/null | sed 's/\/24//' | tr -d '\n'
-echo ""
-echo -n "        - MAC address  : "
-ip link show | grep "link/ether" | awk '{print $2}' 2>/dev/null | tr -d '\n'
-echo ""
+## Network ##
+IP=`ip addr | grep "enp0s3" | awk 'FNR==2 {print $2}' 2>/dev/null | sed 's/\/24//'`
+MAC=`ip link show | grep "link/ether" | awk '{print $2}' 2>/dev/null`
 
-###### Sudo ######
-echo -n "  # Sudo            : "
-cat /var/log/sudo/sudo.log 2>/dev/null | grep "COMMAND" | wc -l | tr -d '\n'
-echo " cmd"
+## Sudo ##
+SUDO=`cat /var/log/sudo/sudo.log 2>/dev/null | grep "COMMAND" | wc -l`
+
+#### Printout ####
+wall "  # Architecture    :
+        - operating system : $OS
+	- architecture     : $ARCH
+	- kernel version   : $KERNEL
+  # CPU physical    : $CPU_PHYSI
+  # vCPU            : $VCPU
+  # Memory Usage    : $MEMORY_USAGE
+  # Disk Usage      : $DISK_USAGE
+  # CPU load        : $CPU_LOAD
+  # Last boot       : $LAST_BOOT
+  # LVM use         : $LVM_USE
+  # Connections TCP : $CONN_TCP (ESTABLISHED)
+  # User log        : $USER_LOG
+  # Network         :
+ 	- IPv4 address : $IP
+	- MAC address : $MAC
+  # Sudo            : $SUDO cmd"
